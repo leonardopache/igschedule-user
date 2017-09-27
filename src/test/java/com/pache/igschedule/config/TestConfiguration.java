b0d7460f49dd0fb8531864cb2db7b786a0b992b1
leonardopache <leonardo@pache.eng.br>
2017-09-28 01:04:41 +0200

    correct test with @AutoWired

--- src/test/java/com/pache/igschedule/config/TestConfiguration.java
+++ src/test/java/com/pache/igschedule/config/TestConfiguration.java
@@ -1,13 +1,9 @@
 package com.pache.igschedule.config;
 
-import static org.mockito.Mockito.mock;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import com.pache.igscheduleuser.IgscheduleuserApplication;
-import com.pache.igscheduleuser.repository.UserRepository;
 
 /**
  * Created by lpache on 7/18/17.
@@ -15,9 +11,5 @@ import com.pache.igscheduleuser.repository.UserRepository;
 @Configuration
 @Import({IgscheduleuserApplication.class})
 public class TestConfiguration {
-    
-    @Bean
-    public UserRepository userRepository() {
-        return mock(UserRepository.class);
-    }
+
 }
--- src/test/java/com/pache/igschedule/controller/UserControllerTest.java
+++ src/test/java/com/pache/igschedule/controller/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.pache.igschedule.controller;
 
-import static org.junit.Assert.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -12,47 +13,55 @@ import javax.ws.rs.core.MediaType;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pache.igschedule.config.TestConfiguration;
+import com.pache.igscheduleuser.controller.UserController;
+import com.pache.igscheduleuser.controller.resourcesuporte.UserList;
 import com.pache.igscheduleuser.entity.User;
 import com.pache.igscheduleuser.repository.UserRepository;
 
 /**
  * Created by lpache on 7/18/17.
  */
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = TestConfiguration.class)
 public class UserControllerTest {
 
 	@Autowired
 	private WebApplicationContext context;
-	
-//	@Mock
+
+	// inject on controller our Mocked repository
 	@Autowired
+	@InjectMocks
+	private UserController userController;
+
+	@Mock
 	private UserRepository repository;
 
 	private MockMvc mvc;
-	private User user;
-	
+
 	@Before
 	public void setUp() {
-		user = newInstaceUser("Leonardo", "Pache");
 		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
-		
-//		MockitoAnnotations.initMocks(this);
+		MockitoAnnotations.initMocks(this);
 	}
 
-	private User newInstaceUser(String name, String lastName) {
+	private User newInstaceUser(long userId, String name, String lastName) {
 		User user = new User();
+		user.setUserId(userId);
 		user.setName(name);
 		user.setLastName(lastName);
 		return user;
@@ -60,56 +69,47 @@ public class UserControllerTest {
 
 	@Test
 	public void getNameController() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/users/base")
-				.accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().is(200));
+		assertThat(mvc.perform(MockMvcRequestBuilders.get("/users/base"))
+				.andReturn()	.getResponse().getStatus())
+				.isEqualTo(200);
 	}
 
 	@Test
 	public void getAll() throws Exception {
 		List<User> listUser = new ArrayList<>();
-		User newInstaceUser = newInstaceUser("Leonardo", "Pache");
-		listUser.add(newInstaceUser);
-		UserRepository mock = org.mockito.Mockito.mock(UserRepository.class);
-		when(mock.findByName(newInstaceUser.getName())).thenReturn(listUser);
-		
-		//Mockito.when(repository.findAll()).thenReturn(listUser);
+		listUser.add(newInstaceUser(1l,"Leonardo", "Pache"));
+		when(repository.findAll()).thenReturn(listUser);
 
-		MvcResult getResult = mvc.perform(MockMvcRequestBuilders.get("/users/all")
-				.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().is(200))
-				.andReturn();
+		MvcResult getResult = mvc.perform(MockMvcRequestBuilders.get("/users/all"))
+				.andExpect(status().is(200)).andReturn();
+		
 		String content = getResult.getResponse().getContentAsString();
-		assertNotNull(content);
+		new ObjectMapper().readValue(content, UserList.class);
+		assertThat(new ObjectMapper().readValue(content, UserList.class).getUserList().size()).isEqualTo(1);
 	}
-	
+
 	@Test
 	public void getUserByName() throws Exception {
 		List<User> listUser = new ArrayList<>();
-		User newInstaceUser = newInstaceUser("Leonardo", "Pache");
+		User newInstaceUser = newInstaceUser(1l,"Leonardo", "Pache");
 		listUser.add(newInstaceUser);
 		UserRepository mock = org.mockito.Mockito.mock(UserRepository.class);
 		when(mock.findByName(newInstaceUser.getName())).thenReturn(listUser);
-		//Mockito.when(repository.findByName(newInstaceUser.getName())).thenReturn(listUser);
+		// Mockito.when(repository.findByName(newInstaceUser.getName())).thenReturn(listUser);
 
-		mvc.perform(MockMvcRequestBuilders.get("/users/"+newInstaceUser.getName())
-				.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().is(200))
-				.andReturn();
+		mvc.perform(MockMvcRequestBuilders.get("/users/" + newInstaceUser.getName()).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().is(200)).andReturn();
 	}
 
 	@Test
 	public void postUser() throws Exception {
-		User newUser = user;
-		newUser.setUserId(1L);
-		Mockito.when(repository.save(user)).thenReturn(newUser);
-		MvcResult getResult = mvc.perform(MockMvcRequestBuilders.post("/users/add")
-				.accept(MediaType.APPLICATION_JSON)
-				.content(""))
-				.andExpect(status().is(200))
-				.andReturn();
 		
-		 String content = getResult.getResponse().getContentAsString();
+		Mockito.when(repository.save((User)any(User.class))).thenReturn(newInstaceUser(1L, "Leonardo", "Pache"));
+		MvcResult getResult = mvc
+				.perform(MockMvcRequestBuilders.post("/users/add").accept(MediaType.APPLICATION_JSON).content(""))
+				.andExpect(status().is(200)).andReturn();
+
+		String content = getResult.getResponse().getContentAsString();
 	}
 
 }
